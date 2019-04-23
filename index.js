8589c7fd9296a2b91f3dfccb8f9c198f18ddeaea
Yangyang Liu <heosyamlyy@gmail.com>
2019-04-23 08:06:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ function sayHiToGrandma(input) {
     return "I can't hear you!"
   } else if (input === input.toUpperCase()) {
     return "YES INDEED!"
-  } else if (input === "I love you, Grandma.")s{
+  } else if (input === "I love you, Grandma.") {
     return "I love you, too."
   }
   
